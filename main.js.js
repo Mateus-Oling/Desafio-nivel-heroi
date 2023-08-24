c41d966fd3bbe12974159313394aad24a65408f5
Mateus-Oling <mateus.oling@hotmail.com>
2023-08-24 16:44:45 -0300

    firts commit

---
+++ main.js.js
@@ -0,0 +1,32 @@
+let nomeHeroi = "Master Oling"
+let nivelHeroi = 5000
+
+if (nivelHeroi <= 1000) {
+    console.log("O elo do seu herói é: Ferro")
+} 
+else if (nivelHeroi <= 1001 || nivelHeroi <= 2000) {
+    console.log("O elo do seu herói é: Bronze")
+} 
+else if (nivelHeroi <= 2001 || nivelHeroi <= 5000) {
+    console.log("O elo do seu herói é: Prata")
+} 
+else if (nivelHeroi <= 6001 || nivelHeroi <= 7000) {
+    console.log("O elo do seu herói é: Ouro")
+} 
+else if (nivelHeroi <= 7001 || nivelHeroi <= 8000) {
+    console.log("O elo do seu herói é: Platina")
+} 
+else if (nivelHeroi <= 8001 || nivelHeroi <= 9000) {
+    console.log("O elo do seu herói é: Ascendente")
+} 
+else if (nivelHeroi <= 9001 || nivelHeroi <= 10000) {
+    console.log("O elo do seu herói é: Imortal")
+} 
+else if (nivelHeroi <= 10001) { 
+    console.log("O elo do seu herói é: Radiante")
+}
+
+console.log("O Herói de nome " + nomeHeroi + " possui " + nivelHeroi + " de exp"   ) 
+
+
+
